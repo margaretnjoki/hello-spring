8441d3add4d9ca3e5202308ebea30cea369273cf
margaretnjoki <maggykiroko@gmail.com>
2026-07-07 17:31:18 +0300

    added a power method to multiply the base number by its power

--- src/main/java/com/margaretnjoki/hello_spring/MathController.java
+++ src/main/java/com/margaretnjoki/hello_spring/MathController.java
@@ -1,7 +1,14 @@
 package com.margaretnjoki.hello_spring;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MathController {
+    @GetMapping("/pow/{base}/{exp}")
+    public int power(@PathVariable int base, @PathVariable int exp) {
+        return (int) Math.pow(base, exp);
+    }
 }
